8ed6cb9132b251a45956756c7f0ba2596e2cc3c3
Sandun <SandunWebDev@gmail.com>
2021-02-24 12:10:39 +0530

    test 6

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function HelloWorld() {
-  return /*#__PURE__*/React.createElement("h1", null, "Hello World4");
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World5");
 }
 
 export { HelloWorld };
